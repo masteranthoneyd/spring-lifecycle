3320414aec302fbed4e0f3394e5289ab950b6f10
Bingdong Yang <bingdong.yang@salesforce-china.com>
2024-07-11 15:51:37 +0800

    add native image support

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,14 @@ plugins {
     idea
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "com.yangbingdong.spring.explore"
 version = "0.0.1-SNAPSHOT"
 
 java {
+    sourceCompatibility = JavaVersion.VERSION_21
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
@@ -32,6 +34,28 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+graalvmNative {
+    binaries.all {
+        buildArgs.add("-J-Dfile.encoding=UTF-8")
+        buildArgs.add("-J-Dstdout.encoding=UTF-8")
+        buildArgs.add("-J-Dstderr.encoding=UTF-8")
+        buildArgs.add("-J-Dconsole.encoding=UTF-8")
+        buildArgs.add("-J-Duser.language=en")
+        jvmArgs.add("-Dfile.encoding=UTF-8")
+        jvmArgs.add("-Dstdout.encoding=UTF-8")
+        jvmArgs.add("-Dstderr.encoding=UTF-8")
+        jvmArgs.add("-Dconsole.encoding=UTF-8")
+        runtimeArgs.add("-Dfile.encoding=UTF-8")
+        runtimeArgs.add("-Dstdout.encoding=UTF-8")
+        runtimeArgs.add("-Dstderr.encoding=UTF-8")
+        runtimeArgs.add("-Dconsole.encoding=UTF-8")
+    }
 }
 
 idea {
